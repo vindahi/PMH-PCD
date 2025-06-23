c0a358d0c84e799edc98096432c3b4e1f205b819
FanFan <140507577+vindahi@users.noreply.github.com>
2025-06-23 08:18:00 +0800

    Update model.py

--- model.py
+++ model.py
@@ -90,9 +90,6 @@ class PCH(nn.Module):
         self.batch_size = len(image)
         imageH = self.imageMLP(image)#nbit length
         textH = self.textMLP(text)
-        
-        # pimage = self.pro(image_feat) + torch.randn_like(self.pro(image_feat)) * 1
-        # ptext = self.pro(text_feat) + torch.randn_like(self.pro(text_feat)) * 1
 
         pimage = self.pro(imageH) 
         ptext = self.pro(textH) 
@@ -103,91 +100,6 @@ class PCH(nn.Module):
         # nec_vec = self.neck(cfeat_concat)     
         code = self.hash_output(cfeat_concat)   
         return pimage, ptext, code, self.classify(code)
-
-
-
-
-
-
-# class PCH(nn.Module):
-#     def __init__(self, args):
-#         super(PCH, self).__init__()
-#         self.image_dim = args.image_dim
-#         self.text_dim = args.text_dim
-
-#         self.img_hidden_dim = args.img_hidden_dim
-#         self.txt_hidden_dim = args.txt_hidden_dim
-#         self.common_dim = args.img_hidden_dim[-1]
-#         self.nbit = int(args.nbit)
-#         self.classes = args.classes
-#         self.batch_size = args.batch_size
-        
-#         assert self.img_hidden_dim[-1] == self.txt_hidden_dim[-1]
-
-#         # self.dropout = args.dropout
-#         self.fusionnn = Fusion(fusion_dim=self.common_dim, nbit=self.nbit)
-
-#         self.imageMLP = MLP(hidden_dim=self.img_hidden_dim, act=nn.Tanh(),dropout=args.mlpdrop)
-
-#         self.textMLP = MLP(hidden_dim=self.txt_hidden_dim, act=nn.Tanh(),dropout=args.mlpdrop)
-
-#         self.ifeat_gate = nn.Sequential(
-#             nn.Linear(self.common_dim, self.common_dim*2),
-#             nn.ReLU(),
-#             nn.Linear(self.common_dim*2, self.common_dim), 
-#             nn.Sigmoid())
-#         self.tfeat_gate = nn.Sequential(
-#             nn.Linear(self.common_dim, self.common_dim*2),
-#             nn.ReLU(),
-#             nn.Linear(self.common_dim*2, self.common_dim),
-#             nn.Sigmoid())
-#         self.pro = nn.Sequential(
-#             nn.Linear(self.common_dim, self.common_dim*2),
-#             nn.ReLU(),
-#             nn.Linear(self.common_dim*2, self.nbit), 
-#             nn.BatchNorm1d(self.nbit),
-#             nn.Sigmoid())
-#         self.activation = nn.ReLU()
-#         self.neck = nn.Sequential(
-#             nn.Linear(self.common_dim,self.common_dim*4),
-#             nn.ReLU(),
-#             nn.Dropout(args.dropout),
-#             nn.Linear(self.common_dim*4,self.common_dim)
-#         )
-
-#         self.fusion_layer = nn.Sequential(
-#             nn.Linear(self.nbit, self.common_dim),
-#             nn.ReLU()
-#         )
-
-#         self.hash_output = nn.Sequential(
-#             nn.Linear(self.common_dim, self.nbit),
-#             nn.Tanh())
-#         self.classify = nn.Linear(self.nbit, self.classes)
-
-#     def forward(self, image, text, tgt=None):
-#         self.batch_size = len(image)
-#         imageH = self.imageMLP(image)#nbit length
-#         textH = self.textMLP(text)
-#         ifeat_info = self.ifeat_gate(imageH)#nbit length
-#         tfeat_info = self.tfeat_gate(textH)
-#         image_feat = ifeat_info*imageH
-#         text_feat = tfeat_info*textH
-        
-#         # pimage = self.pro(image_feat) + torch.randn_like(self.pro(image_feat)) * 1
-#         # ptext = self.pro(text_feat) + torch.randn_like(self.pro(text_feat)) * 1
-
-#         pimage = self.pro(image_feat) 
-#         ptext = self.pro(text_feat) 
-#         fused_fine = self.fusionnn(image_feat, text_feat)
-
-#         cfeat_concat = self.fusion_layer(fused_fine)
-#         cfeat_concat = self.activation(cfeat_concat)
-#         nec_vec = self.neck(cfeat_concat)     
-#         code = self.hash_output(nec_vec)   
-#         return pimage, ptext, code, self.classify(code)
-
-
 
 
 class classone(torch.nn.Module):
